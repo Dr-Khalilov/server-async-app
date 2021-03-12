9c0b804363db1d7095875417fb33f29321daf1d2
Umar Khalilov <ERMASTER100@gmail.com>
2021-03-12 17:04:54 +0200

    Add express

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
-const http = require('http')
+const express = require('express')
 
-const hostname = '127.0.0.1'
-const port = 3000
-let reqCount = 0
+const app = express() // create server express
+const users = []
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200
-  console.log(reqCount++)
-  res.setHeader('Content-Type', 'text/plain')
-  res.end(`Hello World ${reqCount}`)
+/* METHOD PATH */
+app.get('/', (req, res) => {
+  console.log('request accepted')
+  res.send('<h1>Hi node express</h1>')
 })
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`)
+app.post('/', (req, res) => {
+  console.log('post method')
+  res.send('POST query')
 })
+
+app.delete('/', (req, res) => {
+  console.log('delete method')
+  res.send('<h2>DELETE query</h2>')
+})
+
+app.put('/', (req, res) => {
+  console.log('update message')
+  res.send('UPDATE query')
+})
+
+app.patch('/', (req, res) => {
+  console.log('edit message')
+  res.send('EDIT query')
+})
+
+app.listen(3000, () => {
+  console.log('Server started')
+}) // start server
